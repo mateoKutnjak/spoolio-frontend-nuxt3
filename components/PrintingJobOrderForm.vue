b948fbeed52476a545ca5885da455f7899e9e611
mateoKutnjak <mateo.kutnjak7@gmail.com>
2022-11-21 21:25:40 +0100

    Dummy job order form

--- components/PrintingJobOrderForm.vue
+++ components/PrintingJobOrderForm.vue
@@ -1,136 +1,77 @@
 <template>
 
-  <p>Here you can order a printing job</p>
-
-  <div class="w-full">
-    <form
-      v-on:submit.prevent="submitLoginForm"
-      class="rounded px-8 pt-6 pb-6"
+  <div class="m-auto align-middle">
+    <FormKit
+      type="form"
+      id="register-form"
+      :form-class="submitted ? 'hide' : 'show'"
+      submit-label="Sign In"
+      @submit="submitHandler"
+      :actions="false"
     >
-      <p>Below is optional part if user is not logged in</p>
-      <br>
-      <div
-        class="mb-4"
-        v-show="!getUser"
-      >
-        <label
-          class="block text-gray-700 text-sm font-bold mb-2"
-          for="name"
-        >
-          Name
-        </label>
-        <input
+      <div class="double">
+        <FormKit
           type="text"
-          name="username"
-          id="user"
-          v-model="username"
-          placeholder="First and last name"
-          class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-        >
+          name="first_name"
+          label="First name"
+          v-model="firstName"
+          :validation="required"
+          validation-visibility="blur"
+        />
+        <FormKit
+          type="text"
+          name="last_name"
+          label="Last name"
+          v-model="lastName"
+          validation="required"
+          validation-visibility="blur"
+        />
       </div>
-      <div
-        class="mb-4"
-        v-show="!getUser"
-      >
-        <label
-          class="block text-gray-700 text-sm font-bold mb-2"
-          for="email"
-        >
-          Email
-        </label>
-        <input
-          type="email"
-          name="email"
-          id="user"
-          v-model="email"
-          placeholder="E-mail"
-          class="block bg-white p-2.5 w-full text-sm text-gray-900 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-        >
-      </div>
-      <div
-        class="mb-4"
-        v-show="!getUser"
-      >
-        <label
-          class="block text-gray-700 text-sm font-bold mb-2"
-          for="phone_number"
-        >
-          Phone number (TODO)
-        </label>
-        <input
-          type=""
-          name="phone_number"
-          id="phone_number"
-          v-model="phoneNumber"
-          placeholder="Phone number"
-          class="block bg-white p-2.5 w-full text-sm text-gray-900 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-        >
-      </div>
-      <p>Below is REQUIRED part</p>
-      <br>
-      <div class="mb-4">
-        <label
-          for="message"
-          class="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
-        >Your message</label>
-        <textarea
-          id="comments"
-          rows="4"
-          name="comments"
-          v-model="comments"
-          class="block bg-white p-2.5 w-full text-sm text-gray-900 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-          placeholder="Write your thoughts here..."
-        ></textarea>
-
-      </div>
-      <div class="mb-4">
-        <label
-          for="message"
-          class="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
-        >Files</label>
-        <div class="flex items-center justify-center w-full">
-          <label
-            for="dropzone-file"
-            class="flex flex-col items-center justify-center w-full h-64 border-2 border-gray-300 border-dashed rounded-lg cursor-pointer bg-gray-50 dark:hover:bg-bray-800 dark:bg-gray-700 hover:bg-gray-100 dark:border-gray-600 dark:hover:border-gray-500 dark:hover:bg-gray-600"
-          >
-            <div class="flex flex-col items-center justify-center pt-5 pb-6">
-              <svg
-                aria-hidden="true"
-                class="w-10 h-10 mb-3 text-gray-400"
-                fill="none"
-                stroke="currentColor"
-                viewBox="0 0 24 24"
-                xmlns="http://www.w3.org/2000/svg"
-              >
-                <path
-                  stroke-linecap="round"
-                  stroke-linejoin="round"
-                  stroke-width="2"
-                  d="M7 16a4 4 0 01-.88-7.903A5 5 0 1115.9 6L16 6a5 5 0 011 9.9M15 13l-3-3m0 0l-3 3m3-3v12"
-                ></path>
-              </svg>
-              <p class="mb-2 text-sm text-gray-500 dark:text-gray-400"><span class="font-semibold">Click to upload</span> or drag and drop</p>
-              <p class="text-xs text-gray-500 dark:text-gray-400">STL, GCODE, PNG, JPG (MAX. 800x400px)</p>
-            </div>
-            <input
-              id="dropzone-file"
-              type="file"
-              class="hidden"
-            />
-          </label>
-        </div>
-      </div>
-
-      <div class="flex items-center">
-        <button
-          class="inline-flex justify-center rounded-md border border-transparent bg-blue-100 px-4 py-2 text-sm font-medium text-blue-900 hover:bg-blue-200 focus:outline-none focus-visible:ring-2 focus-visible:ring-blue-500 focus-visible:ring-offset-2"
-          type="submit"
-        >
-          Request
-        </button>
-      </div>
-    </form>
-
+      <FormKit
+        type="text"
+        name="address"
+        label="Address"
+        v-model="address"
+        validation="required"
+        validation-visibility="blur"
+      />
+      <FormKit
+        type="tel"
+        label="Phone number"
+        placeholder="xxx-xxx-xxxx"
+        validation="matches:/^[0-9]{3}-[0-9]{3}-[0-9]{4}$/"
+        :validation-messages="{
+    matches: 'Phone number must be in the format xxx-xxx-xxxx',
+  }"
+        validation-visibility="dirty"
+      />
+      <FormKit
+        type="email"
+        name="email"
+        label="Email"
+        v-model="email"
+        validation="required|email"
+        validation-visibility="blur"
+      />
+      <FormKit
+        type="textarea"
+        label="Additional information"
+        rows="5"
+        placeholder="Tell us more about your"
+        help="I'll know if you didn't read the book!"
+      />
+      <FormKit
+        type="file"
+        label="Files"
+        accept=".pdf,.doc,.docx,.xml,.md,.csv"
+        help="Upload files"
+        multiple
+      />
+      <FormKit
+        type="submit"
+        label="Request"
+      />
+    </FormKit>
   </div>
 </template>
 
--- formkit.config.js
+++ formkit.config.js
@@ -7,7 +7,7 @@ export default {
             text: {
                 outer: 'mb-5',
                 label: 'block mb-2 text-sm font-medium text-gray-900 dark:text-white',
-                inner: 'max-w-md border border-grey-400 rounded-lg mb-1 overflow-hidden focus-within:border-blue-500',
+                inner: 'border border-grey-400 rounded-lg mb-1 overflow-hidden focus-within:border-blue-500',
                 input: 'w-full h-10 px-3 border-none text-base text-gray-700 placeholder-gray-400',
                 help: 'text-xs text-gray-500',
                 messages: 'list-none p-0 mt-1 mb-0',
@@ -16,7 +16,7 @@ export default {
             email: {
                 outer: 'mb-5',
                 label: 'block mb-2 text-sm font-medium text-gray-900 dark:text-white',
-                inner: 'max-w-md border border-red-400 rounded-lg mb-1 overflow-hidden focus-within:border-blue-500',
+                inner: 'border border-red-400 rounded-lg mb-1 overflow-hidden focus-within:border-blue-500',
                 input: 'w-full h-10 px-3 border-none text-base text-gray-700 placeholder-gray-400',
                 help: 'text-xs text-gray-500',
                 messages: 'list-none p-0 mt-1 mb-0',
@@ -25,8 +25,35 @@ export default {
             password: {
                 outer: 'mb-5',
                 label: 'block mb-2 text-sm font-medium text-gray-900 dark:text-white',
-                inner: 'max-w-md border border-red-400 rounded-lg mb-1 overflow-hidden focus-within:border-blue-500',
+                inner: 'border border-red-400 rounded-lg mb-1 overflow-hidden focus-within:border-blue-500',
                 input: 'w-full h-10 px-3 border-none text-base text-gray-700 placeholder-gray-400',
+                help: 'text-xs text-gray-500',
+                messages: 'list-none p-0 mt-1 mb-0',
+                message: 'text-red-500 mb-1 text-xs'
+            },
+            tel: {
+                outer: 'mb-5',
+                label: 'block mb-2 text-sm font-medium text-gray-900 dark:text-white',
+                inner: 'border border-red-400 rounded-lg mb-1 overflow-hidden focus-within:border-blue-500',
+                input: 'w-full h-10 px-3 border-none text-base text-gray-700 placeholder-gray-400',
+                help: 'text-xs text-gray-500',
+                messages: 'list-none p-0 mt-1 mb-0',
+                message: 'text-red-500 mb-1 text-xs'
+            },
+            textarea: {
+                outer: 'mb-5',
+                label: 'block mb-2 text-sm font-medium text-gray-900 dark:text-white',
+                inner: 'border border-red-400 rounded-lg mb-1 overflow-hidden focus-within:border-blue-500',
+                input: 'w-full px-3 border-none text-base text-gray-700 placeholder-gray-400',
+                help: 'text-xs text-gray-500',
+                messages: 'list-none p-0 mt-1 mb-0',
+                message: 'text-red-500 mb-1 text-xs'
+            },
+            file: {
+                outer: 'mb-5',
+                label: 'block mb-2 text-sm font-medium text-gray-900 dark:text-white',
+                inner: 'border border-red-400 rounded-lg mb-1 overflow-hidden focus-within:border-blue-500',
+                input: 'w-full px-3 border-none text-base text-gray-700 placeholder-gray-400',
                 help: 'text-xs text-gray-500',
                 messages: 'list-none p-0 mt-1 mb-0',
                 message: 'text-red-500 mb-1 text-xs'
@@ -34,7 +61,7 @@ export default {
             submit: {
                 outer: 'mb-5',
                 label: 'block mb-2 text-sm font-medium text-gray-900 dark:text-white',
-                inner: 'max-w-md border border-red-400 rounded-lg mb-1 overflow-hidden focus-within:border-blue-500',
+                inner: 'border border-red-400 rounded-lg mb-1 overflow-hidden focus-within:border-blue-500',
                 input: 'w-full h-10 px-3 border-none text-base text-gray-700 placeholder-gray-400',
                 help: 'text-xs text-gray-500',
                 messages: 'list-none p-0 mt-1 mb-0',
