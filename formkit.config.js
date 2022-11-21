60fb821531b37018a2af4918c31ba01068a18433
mateoKutnjak <mateo.kutnjak7@gmail.com>
2022-11-21 17:57:15 +0100

    FormKit configuration bugfix

--- formkit.config.js
+++ formkit.config.js
@@ -6,7 +6,7 @@ export default {
             text: {
                 outer: 'mb-5',
                 label: 'block mb-1 font-bold text-sm',
-                inner: 'max-w-md border border-red-400 rounded-lg mb-1 overflow-hidden focus-within:border-blue-500',
+                inner: 'max-w-md border border-grey-400 rounded-lg mb-1 overflow-hidden focus-within:border-blue-500',
                 input: 'w-full h-10 px-3 border-none text-base text-gray-700 placeholder-gray-400',
                 help: 'text-xs text-gray-500',
                 messages: 'list-none p-0 mt-1 mb-0',
@@ -22,6 +22,15 @@ export default {
                 message: 'text-red-500 mb-1 text-xs'
             },
             password: {
+                outer: 'mb-5',
+                label: 'block mb-1 font-bold text-sm',
+                inner: 'max-w-md border border-red-400 rounded-lg mb-1 overflow-hidden focus-within:border-blue-500',
+                input: 'w-full h-10 px-3 border-none text-base text-gray-700 placeholder-gray-400',
+                help: 'text-xs text-gray-500',
+                messages: 'list-none p-0 mt-1 mb-0',
+                message: 'text-red-500 mb-1 text-xs'
+            },
+            submit: {
                 outer: 'mb-5',
                 label: 'block mb-1 font-bold text-sm',
                 inner: 'max-w-md border border-red-400 rounded-lg mb-1 overflow-hidden focus-within:border-blue-500',
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -7,7 +7,7 @@ export default defineNuxtConfig({
     css: ['~/assets/css/tailwind.css'],
     tailwindcss: {
         cssPath: '~/assets/css/tailwind.css',
-        configPath: '~/tailwind.config.ts',
+        configPath: '~/tailwind.config.js',
         exposeConfig: false,
         injectPosition: 0,
         viewer: true,
