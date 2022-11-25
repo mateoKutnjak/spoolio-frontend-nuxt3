5d265c10220fd675a72e5e1408ca429e31d60440
mateoKutnjak <mateo.kutnjak7@gmail.com>
2022-11-25 21:35:35 +0100

    Blog buttons fix

--- components/BlogListTile.vue
+++ components/BlogListTile.vue
@@ -1,29 +1,47 @@
 <template>
-  <div class="card lg:card-side bg-base-100 shadow-xl">
+  <div class="card md:card-side bg-base-100 shadow-md">
     <figure><img
-        src="https://placeimg.com/400/400/arch"
+        src="https://placeimg.com/300/300/arch"
         alt="Album"
       /></figure>
     <div class="card-body">
       <h2 class="card-title">{{blog.title}}</h2>
       <p>Click the button to listen on Spotiwhy app.</p>
       <div class="card-actions justify-end gap-3">
-        <button class="btn btn-outline gap-1">
+        <button
+          v-if="isStaffUser"
+          class="btn btn-outline gap-1"
+        >
           <Icon
             name="material-symbols:edit-outline"
             size="20"
             aria-hidden="true"
           />Edit
         </button>
-        <button class="btn btn-primary">Read more</button>
+        <NuxtLink :to="`/blogs/${blog.id}`"><button class="btn btn-primary">Read more</button></NuxtLink>
       </div>
     </div>
   </div>
 
 </template>
   
-  <script setup>
+  <script lang="ts" setup>
+import { useAuthStore } from "~~/stores/auth";
+
+const authStore = useAuthStore();
+
 const { blog } = defineProps(["blog"]); // props
+
+const isStaffUser = ref<boolean>(false);
+
+const getUser = computed(() => {
+  isStaffUser.value = authStore.getUser?.is_staff || false;
+  return authStore.getUser;
+});
+
+watch(getUser, (value, old, invalidate) => {
+  isStaffUser.value = value?.is_staff || false;
+});
 </script>
   
   <style>
