cdf0254c3b1eec8450cd188aa2c1eee9618dd4a7
mateoKutnjak <mateo.kutnjak7@gmail.com>
2022-11-29 12:22:30 +0100

    File formkit element hiddem under drag&drop

--- pages/job-order/index.vue
+++ pages/job-order/index.vue
@@ -1,54 +1,71 @@
 <template>
   <FormKit
-    v-if="false"
-    type="file"
-    label="Your files"
-    help="This input starts with files already “attached”."
-    multiple
-    v-model="formkitFiles"
-  />
-
-  <div
-    class="flex w-full h-64"
-    @dragover.prevent
-    @drop.prevent
+    type="form"
+    id="job-order-dummy-form"
+    :form-class="submitted ? 'hide' : 'show'"
+    submit-label="Sign In"
+    @submit="submitHandler"
+    :actions="false"
   >
-    <label
-      for="dropzone-file"
-      class="flex-1 flex flex-col items-center justify-center w-full h-64 border-2 border-gray-300 border-dashed rounded-lg cursor-pointer bg-gray-50 dark:hover:bg-bray-800 dark:bg-gray-700 hover:bg-gray-100 dark:border-gray-600 dark:hover:border-gray-500 dark:hover:bg-gray-600"
-      @drop="drop"
-    >
-      <div class="mb-2">
-        <Icon
-          color="gray"
-          name="ic:outline-cloud-upload"
-          size="30"
-          aria-hidden="true"
-        />
-      </div>
-      <p class="mb-2 text-sm text-gray-500 dark:text-gray-400"><span class="font-semibold">Click to upload</span> or drag and drop</p>
-      <p class="text-xs text-gray-500 dark:text-gray-400">SVG, PNG, JPG or GIF (MAX. 800x400px) [TODO change]</p>
-      <input
-        id="dropzone-file"
-        type="file"
-        class="hidden"
-        @change="change"
-      />
-    </label>
-  </div>
 
-  <ul class="m-4 space-y-1 max-w-md list-inside text-gray-500 dark:text-gray-400">
-    <li
-      class="flex items-center gap-4"
-      v-for="uploadedFile in uploadedFiles"
-      :key="uploadedFile.name"
+    <div
+      class="flex w-full h-64"
+      @dragover.prevent
+      @drop.prevent
     >
-      <FileUploadListTile
-        :file="uploadedFile"
-        @on-remove-file="removeFile(uploadedFile)"
-      />
-    </li>
-  </ul>
+      <label
+        for="dropzone-file"
+        class="flex-1 flex flex-col items-center justify-center w-full h-64 border-2 border-gray-300 border-dashed rounded-lg cursor-pointer bg-gray-50 dark:hover:bg-bray-800 dark:bg-gray-700 hover:bg-gray-100 dark:border-gray-600 dark:hover:border-gray-500 dark:hover:bg-gray-600"
+        @drop="drop"
+      >
+        <div class="mb-2">
+          <Icon
+            color="gray"
+            name="ic:outline-cloud-upload"
+            size="30"
+            aria-hidden="true"
+          />
+        </div>
+        <p class="mb-2 text-sm text-gray-500 dark:text-gray-400"><span class="font-semibold">Click to upload</span> or drag and drop</p>
+        <p class="text-xs text-gray-500 dark:text-gray-400">SVG, PNG, JPG or GIF (MAX. 800x400px) [TODO change]</p>
+        <input
+          id="dropzone-file"
+          type="file"
+          class="hidden"
+          @change="change"
+        />
+      </label>
+    </div>
+
+    <FormKit
+      type="file"
+      help="This input starts with files already “attached”."
+      multiple
+      v-model="uploadedFiles"
+      validation="required"
+      :validation-messages="{
+        required: 'Files are required',
+      }"
+    />
+
+    <ul class="m-4 space-y-1 max-w-md list-inside text-gray-500 dark:text-gray-400">
+      <li
+        class="flex items-center gap-4"
+        v-for="uploadedFile in uploadedFiles"
+        :key="uploadedFile.name"
+      >
+        <FileUploadListTile
+          :file="uploadedFile"
+          @on-remove-file="removeFile(uploadedFile)"
+        />
+      </li>
+    </ul>
+
+    <FormKit
+      type="submit"
+      label="Next"
+    />
+  </FormKit>
 
 </template>
   
@@ -61,6 +78,8 @@ const uploadedFiles = ref<File[]>([]);
 
 const files = ref<any>([]);
 const formkitFiles = ref<any>([]);
+
+const submitted = ref(false);
 
 const getUser = computed(() => {
   return authStore.getUser;
@@ -109,6 +128,13 @@ function removeFile(file: File) {
     uploadedFiles.value.splice(index, 1);
   }
 }
+
+const submitHandler = async () => {
+  // This delay is here only because of progress indicator button
+  await new Promise((r) => setTimeout(r, 1000));
+
+  submitted.value = true;
+};
 </script>
   
   <style>
