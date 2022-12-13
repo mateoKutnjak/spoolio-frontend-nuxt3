e8c7c72576294adec60fc1110b004a35a3f590fc
mateoKutnjak <mateo.kutnjak7@gmail.com>
2022-12-13 12:21:55 +0100

    Toast bugfix

--- constants/constants.ts
+++ constants/constants.ts
@@ -2,3 +2,5 @@ export const HTTP_REQUEST_TIMEOUT = 5000;
 
 export const CONTENT_TYPE_ORDER_UNIT = 'orderunit';
 export const CONTENT_TYPE_ORDER = 'printorder';
+
+export const TOAST_DURATION_MEDIUM = 5000;
