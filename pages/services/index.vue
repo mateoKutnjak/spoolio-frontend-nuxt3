0568695ddffb9b4fd265552a3490de37072c6e37
mateoKutnjak <mateo.kutnjak7@gmail.com>
2022-12-26 00:19:30 +0100

    Design tweaks

--- pages/services/index.vue
+++ pages/services/index.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="container px-12 py-6">
     <div class="px-12 py-8 flex flex-col gap-12">
-      <div class="flex flex-col md:flex-row gap-10 justify-between items-stretch md:items-center">
+      <div class="flex flex-col lg:flex-row gap-10 justify-between items-stretch lg:items-center">
         <div class="flex-1 flex flex-col gap-3 text-center">
           <NuxtLink to="/services/printing">
             <div class="btn btn-outline bg-white hover:btn-info w-full h-[32vh] border-gray-400">
@@ -14,9 +14,8 @@
               </div>
             </div>
           </NuxtLink>
-          <h1 class="text-md">I have some files I want to 3d print</h1>
+          <h1 class="text-lg italic">"Print some 3D files for me"</h1>
         </div>
-        <h1 class="text-2xl font-bold text-center pb-4 md:pb-8">OR</h1>
         <div class="flex-1 flex flex-col gap-3 text-center">
           <NuxtLink to="/services/modeling">
             <div class="btn btn-outline bg-white hover:btn-success w-full h-[32vh] border-gray-400">
@@ -29,7 +28,7 @@
               </div>
             </div>
           </NuxtLink>
-          <h1 class="text-md">I have an idea & need your 3d design</h1>
+          <h1 class="text-lg italic">"Help me create my 3D idea"</h1>
         </div>
       </div>
     </div>
