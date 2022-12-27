c7f02b37beb3fc0566e9ba3741e4ea5f8c437095
mateoKutnjak <mateo.kutnjak7@gmail.com>
2022-12-27 21:21:02 +0100

    Design tweaks

--- formkit.config.js
+++ formkit.config.js
@@ -3,8 +3,11 @@ import { createAutoAnimatePlugin } from '@formkit/addons'
 
 const textClassification = {
     label: 'w-full lock mb-2 text-sm font-medium text-gray-800',
-    inner: 'w-full bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 focus:outline-none overflow-hidden',
+    inner: 'w-full bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-xl focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 focus:outline-none overflow-hidden',
     input: 'w-full bg-transparent focus:outline-none',
+    wrapper: 'shadow-xs rounded-xl',
+    outer: 'rounded-xl mb-3',
+    message: 'px-5 pt-1 mb-0'
 }
 const boxClassification = {
     fieldset: 'max-w-md border border-gray-400 rounded-md px-2 pb-1',
