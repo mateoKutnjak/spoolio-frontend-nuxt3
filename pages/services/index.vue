71e49b1e342c60ad7c8e95332b8202775347efd8
mateoKutnjak <mateo.kutnjak7@gmail.com>
2023-01-08 20:48:44 +0100

    Code clean

--- pages/services/index.vue
+++ pages/services/index.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="container px-12 py-12">
     <div class="flex flex-wrap gap-10 justify-center">
-      <div class="flex flex-col gap-3 text-center">
+      <div class="flex flex-col gap-3 text-center w-52">
         <NuxtLink to="/services/printing">
           <div class="btn btn-info h-44 w-52">
             <div class="flex flex-col gap-3 items-center text-gray-800">
@@ -15,7 +15,7 @@
         </NuxtLink>
         <h1 class="text-md italic font-medium text-gray-500">"Print some 3D files for me"</h1>
       </div>
-      <div class="flex flex-col gap-3 text-center">
+      <div class="flex flex-col gap-3 text-center w-52">
         <NuxtLink to="/services/modeling">
           <div class="btn btn-success h-44 w-52">
             <div class="flex flex-col gap-3 items-center text-gray-800">
@@ -29,7 +29,7 @@
         </NuxtLink>
         <h1 class="text-md italic font-medium text-gray-500">"Help me create my 3D idea"</h1>
       </div>
-      <div class="flex flex-col gap-3 text-center">
+      <div class="flex flex-col gap-3 text-center w-52">
         <NuxtLink to="/store">
           <div class="btn btn-warning h-44 w-52">
             <div class="flex flex-col gap-3 items-center text-gray-800">
@@ -41,7 +41,7 @@
             </div>
           </div>
         </NuxtLink>
-        <h1 class="text-md italic font-medium text-gray-500">"I want to buy 3D printer products"</h1>
+        <h1 class="text-md italic font-medium text-gray-500">"I want to buy your 3D printer products"</h1>
       </div>
     </div>
   </div>
