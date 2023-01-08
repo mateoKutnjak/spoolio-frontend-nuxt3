62dd2e439178834bb2d9340c57ff601cfaf67ae4
mateoKutnjak <mateo.kutnjak7@gmail.com>
2023-01-08 18:55:54 +0100

    Webshop as service button

--- pages/services/index.vue
+++ pages/services/index.vue
@@ -1,10 +1,10 @@
 <template>
-  <div class="container px-12 py-6">
+  <div class="container px-12 py-12">
     <div class="flex flex-wrap gap-10 justify-center">
-      <div class="flex flex-col gap-3 text-center w-64">
+      <div class="flex flex-col gap-3 text-center">
         <NuxtLink to="/services/printing">
-          <div class="btn btn-outline bg-white hover:btn-info h-52 w-64 border-gray-400">
-            <div class="flex flex-col gap-3 items-center text-gray-600">
+          <div class="btn btn-info h-44 w-52">
+            <div class="flex flex-col gap-3 items-center text-base-600">
               <Icon
                 name="file-icons:3d-model"
                 size="50"
@@ -15,9 +15,9 @@
         </NuxtLink>
         <h1 class="text-md italic font-medium text-gray-500">"Print some 3D files for me"</h1>
       </div>
-      <div class="flex flex-col gap-3 text-center w-64">
+      <div class="flex flex-col gap-3 text-center">
         <NuxtLink to="/services/modeling">
-          <div class="btn btn-outline bg-white hover:btn-success h-52 w-64 border-gray-400">
+          <div class="btn btn-success h-44 w-52">
             <div class="flex flex-col gap-3 items-center text-gray-600">
               <Icon
                 name="material-symbols:design-services"
@@ -28,6 +28,20 @@
           </div>
         </NuxtLink>
         <h1 class="text-md italic font-medium text-gray-500">"Help me create my 3D idea"</h1>
+      </div>
+      <div class="flex flex-col gap-3 text-center">
+        <NuxtLink to="/store">
+          <div class="btn btn-warning h-44 w-52">
+            <div class="flex flex-col gap-3 items-center text-gray-600">
+              <Icon
+                name="mdi:chip"
+                size="50"
+              />
+              Web shop
+            </div>
+          </div>
+        </NuxtLink>
+        <h1 class="text-md italic font-medium text-gray-500">"I want to buy 3D printer products"</h1>
       </div>
     </div>
   </div>
