5757a4c2b61ba288cf5cf3f15bb4b81b781822a5
mateoKutnjak <mateo.kutnjak7@gmail.com>
2023-01-11 11:51:22 +0100

    Blog edit and index tweaks

--- pages/blogs/[id]/edit.vue
+++ pages/blogs/[id]/edit.vue
@@ -1,72 +1,70 @@
 <template>
   <div class="container p-12">
-    <FormKit
-      type="form"
-      id="blog-edit-form"
-      :form-class="submitted ? 'hide' : 'show'"
-      submit-label="Update"
-      @submit="submitHandler"
-      :actions="false"
-    >
-      <div class="flex flex-col">
+    <div>
+      <FormKit
+        type="form"
+        id="blog-edit-form"
+        :form-class="submitted ? 'hide' : 'show'"
+        submit-label="Update"
+        @submit="submitHandler"
+        :actions="false"
+      >
+        <div class="flex flex-col gap-5">
 
-        <FormKit
-          type="text"
-          name="title"
-          label="Title"
-          v-model="blogTitle"
-          validation="required"
-          validation-visibility="blur"
-        />
-        <FormKit
-          type="text"
-          name="subtitle"
-          label="Subtitle"
-          v-model="blogSubtitle"
-          validation="required"
-          validation-visibility="blur"
-        />
-        <div class="grid grid-rows-1 grid-cols-1 md:grid-cols-2 gap-5">
-          <!-- <FormKit
-          class="flex-1 source bg-gray-100"
-          v-show="false"
-          type="textarea"
-          name="content"
-          label="Content"
-          v-model="content"
-          validation="required"
-          validation-visibility="blur"
-        /> -->
-          <textarea
-            class="flex-1 source bg-gray-100 overflow-y h-screen p-4"
-            v-model="blogContent"
-          />
-
-          <div>
-            <div
-              class="prose overflow-y-auto h-screen"
-              v-html="$renderer.render(blogContent)"
-            ></div>
+          <div class="max-w-md">
+            <FormKit
+              type="text"
+              name="title"
+              label="Title"
+              v-model="blogTitle"
+              validation="required"
+              validation-visibility="blur"
+            />
           </div>
-        </div>
-        <div class="card-actions justify-center py-12">
-          <FormKit
-            type="submit"
-            label="Update post"
-          />
-        </div>
+          <div>
+            <FormKit
+              type="text"
+              name="subtitle"
+              label="Subtitle"
+              v-model="blogSubtitle"
+              validation="required"
+              validation-visibility="blur"
+            />
+          </div>
+          <div class="grid grid-rows-1 grid-cols-1 md:grid-cols-2 gap-5">
+            <textarea
+              class="flex-1 source bg-gray-100 overflow-y h-screen p-4"
+              v-model="blogContent"
+            />
 
-      </div>
-    </FormKit>
+            <div>
+              <div
+                class="prose overflow-y-auto h-screen"
+                v-html="$renderer.render(blogContent)"
+              ></div>
+            </div>
+          </div>
+          <div class="card-actions justify-center py-12">
+            <FormKit
+              type="submit"
+              label="Update post"
+            />
+          </div>
+
+        </div>
+      </FormKit>
+    </div>
   </div>
 </template>
 
 <script lang="ts" setup>
 import { useBlogStore } from "~~/stores/blog";
-import IUserResponse, { useAuthStore } from "~~/stores/auth";
+import { IUserResponse, useAuthStore } from "~~/stores/auth";
+import { useNotificationStore } from "~~/stores/notification";
 
 const authStore = useAuthStore();
 const blogStore = useBlogStore();
+const notificationStore = useNotificationStore();
 
 const { id } = useRoute().params;
 
@@ -129,21 +127,13 @@ const submitHandler = async () => {
       blogContent.value
     )
     .then(() => {
-      // TODO check if navigateto dest the job
-      // navigateTo(`/blogs/${id}/`);
+      notificationStore.show("Update saved", ToastLevel.info());
     })
     .catch((err) => {});
 };
 </script>
 
 <style scoped>
-.container {
-  display: inline;
-}
-.container :nth-last-child(1) {
-  display: inline;
-}
-
 .scrollable {
   overflow-y: scroll;
 }
