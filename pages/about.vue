6f2bfb72bc217f2b92ebd8335c044902723be173
mateoKutnjak <mateo.kutnjak7@gmail.com>
2023-03-08 10:46:38 +0100

    Manually removed netlify

--- pages/about.vue
+++ pages/about.vue
@@ -1,8 +1,6 @@
 <template>
   <div class="container p-12">
     <div>About</div>
-    <img src="/.netlify/functions/ipx/w_200/public/img/logo-primary.svg" />
-    <img src="/.netlify/functions/ipx/w_200/img/logo-primary.svg" />
   </div>
 </template>
   
